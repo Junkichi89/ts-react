0cf20209cbde4bceaa4b258d4fb321a489096254
Jumpei Asanuma <jun64pei89@gmail.com>
2023-03-21 22:34:45 +0900

    test firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { collection, addDoc } from "firebase/firestore";
+import { db } from './lib/firebase';
 
 function App() {
+
+  const addSample = async() => {
+const docRef = await addDoc(collection(db, "cities"), {
+  name: "Tokyo",
+  country: "Japan"
+});
+    console.log("Document written with ID: ", docRef.id);
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addSample}>Add Sample</button>
     </div>
   );
 }
